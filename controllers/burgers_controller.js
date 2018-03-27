e0311ad3d1b070ca909c9c9d1dab1be24d2d6782
rebecca8840 <31925939+rebecca8840@users.noreply.github.com>
2018-03-26 23:01:01 -0400

    Add files via upload

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,6 @@
+var express = require("express");
+
+var route = require.Router();
+
+var burger = require("../models/burger.js");
+
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Quinoa and Sweet Potato", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Portobello and Peach", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Blue Cheese and Onion", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Sweet Potato and Black Bean", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Goat Cheese and Onion", true);
