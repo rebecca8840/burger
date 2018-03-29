7611abaf075595fe40d3fa29aa57e76d32277021
rebecca8840 <31925939+rebecca8840@users.noreply.github.com>
2018-03-28 22:42:46 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 
 var app = express();
 
-var PORT = process.env.PORT || 3306;
+var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
